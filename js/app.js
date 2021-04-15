8c96eb7a483e3ace32791e24119477bcf1aee529
mahmoodaltahat <>
2021-04-15 12:55:40 +0000

    looping

--- js/app.js
+++ js/app.js
@@ -1,34 +1,48 @@
 alert('welcome to TFED store');
-  document.write("<pre>")
+  document.write("<pre>");
 var user_name = prompt('please enter your name')
   document.write('welcom ' + user_name);
    console.log('welcom ' + user_name);
-  document.write("<pre>")
-document.write('<p1> THIS IS OUR PRODUCT  </p1>');
+    document.write("<pre>");
  var your_device = prompt('please enter a device type (TV Screen , Labtop)?')
-if(your_device =='TV Screen')
-{
-  var image = '<img src="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/2073/2073039_xcv1a.jpg" alt= "TV"/>';
-    document.write("<pre>")
-  document.write(image);
-  document.write("<pre>")
-  var price= 400;
-    document.write('the price is ' + price + 'JD');
-}else if(your_device =='Labtop')
-{
-  var image2 = '<img src="image/images.jpg" alt= "Labtop"/>';
-  document.write("<pre>")
-  document.write(image2);
-    document.write("<pre>")
-    var price= 700;
-    document.write('the price is ' + price + 'JD');
+
+while (your_device !== 'TV Screen' && your_device !== 'Labtop') {
+  your_device = prompt('please enter a device type (TV Screen , Labtop)?');
 }
+console.log(your_device);
+
 var txt;
 var r = confirm("Press 'OK' if you want to see the product 'cancel' if not  ");
 if (r == true) {
   txt = "contact us on 078000000";
+
+var devices_number= prompt('how many device do you want to Buy?');
+for (var i = 1; i <= devices_number ; i++) 
+{ 
+  if(your_device =='TV Screen')
+  {
+    var image = '<img src="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/2073/2073039_xcv1a.jpg" alt= "TV"/>';
+    document.write("<pre>");
+    document.write(image);
+    document.write("<pre>");
+    var price= 400*devices_number;
+        
+  }   else if(your_device =='Labtop')
+  {
+   var image2 = '<img src="image/images.jpg" alt= "Labtop"/>';
+   document.write("<pre>");
+   document.write(image2);
+    document.write("<pre>");
+    var price= 700*devices_number;
+  }
+}
+
+document.write('The Total Price is ' +  price + 'JD');
+document.write("<pre>");
+document.write('<h3> if want to buy go to contact us  </h3>');
 } else {
   txt = "welcom at our stor";
+  document.write('<p1> ^_^ You are Losing Us ^_^  </p1>');
 }
-document.write("<pre>")
-  document.write('<h3> if want to buy go to contact us  </h3>');
+
+  
